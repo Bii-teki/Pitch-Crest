6abf66891d41b4bc11927d3f51e7eb56eaa239f1
Bii-Teki <geoffrey.bii@student.moringaschool.com>
2023-12-09 12:09:15 +0300

    updating intro page

--- clients/src/Components/Home/Intro.js
+++ clients/src/Components/Home/Intro.js
@@ -1,23 +1,34 @@
+import React from "react";
 
 function Intro() {
-    return (
-
-    <>
-    <div className="sm:mb-10 lg:grid mt-12 lg:grid-cols-5 md:grid-cols-none md:bg-gray-300 bg-gray-300 lg:bg-white lg:h-full">
-      <div className=" px-10 py-10 max-w-md m-auto lg:col-span-2 mt-20 mb-20 shadow-xl rounded-xl lg:mt-10 md:shadow-xl md:rounded-xl lg:shadow-none lg:rounded-none lg:w-full lg:mb-10 lg:px-5 lg:pt-5 lg:pb-5 lg:max-w-lg bg-white">
-        <img className="h-64 sm:h-52 sm:w-full sm:object-cover lg:hidden object-center mt-2 rounded-lg shadow-2xl" src="https://www.myaccelerate.io/assets/utilities/image/epa.jpg" alt="Ad- woman on a beach"/>
-        <h1 className="font-bold text-lg text-gray-600">GET STARTED TODAY!</h1>
-        <h1 className="text-lg text-gray-600 text-justify pt-2">Pitch Crest is an innovative online platform that revolutionizes the way entrepreneurs and startups pitch their projects to potential investors. Our platform facilitates seamless connections between visionary project creators and astute investors seeking promising ventures to fund. We aim to enhance collaboration, foster meaningful relationships, and drive the growth of groundbreaking projects that shape the future.</h1>
-        <button className="mt-5 bg-gray-600 p-3 shadow-2xl rounded-xl text-white font-bold hover:bg-gray-800">GET STARTED!</button>
-      </div>
-
-      <div className="hidden relative lg:block mr-11  lg:col-span-3">
-        <img className="absolute inset-0 w-full h-full object-cover object-center" src="https://www.myaccelerate.io/assets/utilities/image/epa.jpg" alt="Ad- woman on a beach"/>
+  return (
+    <div className="min-h-screen bg-gray-300 flex items-center justify-center">
+      <div className="lg:grid lg:grid-cols-2 max-w-6xl mx-auto p-8 lg:p-16 lg:gap-8">
+        <div className="lg:order-2 mt-4 lg:mt-0"> {/* Adjusted the top margin */}
+          <img
+            className="w-full h-auto lg:h-96 object-cover rounded-xl shadow-2xl"
+            src="https://images.pexels.com/photos/5676744/pexels-photo-5676744.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="Ad- woman on a beach"
+          />
+        </div>
+        <div className="lg:order-1 mt-8 lg:mt-0">
+          <h1 className="font-bold text-3xl lg:text-5xl text-gray-800 mb-4">
+            Elevate Your Project with Pitch Crest
+          </h1>
+          <p className="text-lg lg:text-xl text-gray-600 leading-7">
+            Pitch Crest is your gateway to revolutionizing project pitches.
+            Connect with visionary creators and astute investors. Together, we
+            drive the growth of groundbreaking projects that shape the future.
+          </p>
+          <div className="flex items-center justify-center mt-6">
+            <button className="bg-blue-500 p-4 lg:p-3 rounded-full text-white font-bold hover:bg-blue-700">
+              Get Started Today!
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-        
-        </>
-    )
+  );
 }
 
 export default Intro;
